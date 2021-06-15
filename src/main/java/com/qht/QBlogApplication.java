6ed473dae1c2eec2d39c03804f80933198269ff1
linxiu <2410480275@qq.com>
2021-06-15 17:28:27 +0800

    Initial commit

---
+++ src/main/java/com/qht/QBlogApplication.java
@@ -0,0 +1,13 @@
+package com.qht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QBlogApplication.class, args);
+    }
+
+}
